77d5deb7fb759e7e11aeb8341ec8b7c646248918
ruderbyets <fahmi.j@programmer.net>
2015-10-14 21:10:28 +0700

    Regular Expression uploaded by Git Netbeans

---
+++ src/regex/KarakterApapun.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import java.util.regex.*;
+
+/**
+ *
+ * @author muhammad
+ */
+/*
+.(dot) digunakan untuk mencari semua karakter.
+*/
+public class KarakterApapun {
+    public static void main(String[] args) {
+		String str = "Ahmad Rosid AK 12345 PA (0728) 555-121 Yogyakarta ocittwo@gmail.com 412-555-1234";
+//		String str2 = "1Z aaa {{{ [[[";
+		checkRegex(".", str);
+	}
+
+	public static void checkRegex(String str1, String str2) {
+		Pattern ptr = Pattern.compile(str1);
+		Matcher regexMtcr = ptr.matcher(str2);
+
+		while (regexMtcr.find()) {
+			if (regexMtcr.group().length() != 0) {
+				System.out.println(regexMtcr.group().trim());
+			}
+		}
+	}
+}
---
+++ src/regex/KarakterAsk.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import java.util.regex.*;
+
+/**
+ *
+ * @author muhammad
+ */
+public class KarakterAsk {
+    	public static void main(String[] args) {
+		String str = "Ahmad Rosid abbbc AK 12345 PA (0728) 555-121 Yogyakarta ocittwo@gmail.com 412-555-1234 bbb cc";
+		String str2 = "1Z aaa {{{ [[[ 3123hjkhk";
+		checkRegex("[0-9]+?([a-z])+", str2);
+	}
+ 
+	public static void checkRegex(String str1, String str2) {
+		Pattern ptr = Pattern.compile(str1);
+		Matcher regexMtcr = ptr.matcher(str2);
+ 
+		while (regexMtcr.find()) {
+			if (regexMtcr.group().length() != 0) {
+				System.out.println(regexMtcr.group().trim());
+			}
+		}
+	}
+}
